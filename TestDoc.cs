e19d7089854b05e8a407dca9aec6fb14657b0329
C-Rupp <rupp.christopher1@gmail.com>
2024-10-02 19:42:54 -0400

    Initial Test
    Test

---
+++ TestDoc.cs
@@ -0,0 +1,15 @@
+class Student
+	{
+		private string firstName;
+		private string lastName;
+		private double grade;
+		
+		public string FirstName {
+			get {
+				return firstName;
+				}
+			set {
+				firstName = value;
+			}
+		}
+	}
