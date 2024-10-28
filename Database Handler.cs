1a8e4b3ebaef98519e8c34fb42cb097a838233c5
mandu-u <mandy_ngo@outlook.com>
2024-10-28 19:57:44 -0400

    Add files via upload

--- Database Handler.cs
+++ Database Handler.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Appointment_Mgmt_System___BTM495
 {
+    using System.Data.Common;
+    using System.Data.SqlClient;
+
     public class Database_Handler
     {
+        string connectionString = "Data Source=DESKTOP-MN\\SQLEXPRESS;Initial Catalog=\"BTM495 DB\";Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        
     }
 }
