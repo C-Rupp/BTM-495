0a3bc73d3cb37f46b8e65501ceca241b401a675a
mandu-u <mandy_ngo@outlook.com>
2024-11-18 18:13:58 -0500

    Create CalculateTimeDiff() - Users.cs

---
+++ CalculateTimeDiff() - Users.cs
@@ -0,0 +1,15 @@
+public class Users
+{
+    public int UserID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+
+    public Users(int userID, string firstName, string lastName, string email)
+    {
+        UserID = userID;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+}
